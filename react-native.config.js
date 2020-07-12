f4ee3337f44396e5ae64d385201ea91984cb3d9c
dth <dth@dthlabs.com>
2020-07-12 17:37:19 +0200

    adding custom fonts

---
+++ react-native.config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: [
+    './assets/fonts'
+  ],
+};
+
+/*
+export const project = {
+  ios: {},
+  android: {},
+};
+export const assets = [
+  './assets/fonts',
+];
+
+
+*/
---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View``;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #f4ede8;
+  font-family: 'RotoboSlab-Medium';
+`;
